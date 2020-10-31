d25af435f77d70d426ad2abd3197a21a66cf2273
abam-iksde <assdlik@gmail.com>
2020-10-31 22:44:57 +0100

    added weapon delay

--- scripts/Item.gd
+++ scripts/Item.gd
@@ -6,10 +6,7 @@ var model
 var bullet
 
 func use():
-	var bullet_instance = bullet.instance()
-	print(bullet_instance.transform.origin)
-	get_viewport().add_child(bullet_instance)
-	bullet_instance.global_transform = global_transform
+	pass
 
 
 
