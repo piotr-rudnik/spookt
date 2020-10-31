5561b982825640edbd11ffcc90f1ffa6aac86def
ohai <main>
2020-10-31 21:27:17 +0100

    Whatever

--- scenes/items/Bullet.gd
+++ scenes/items/Bullet.gd
@@ -1,12 +1,14 @@
 class_name Bullet
 extends KinematicBody
 
+var speed = 100
+
 func _ready():
 	print("Bullet spawned")
 	pass
 
 func _physics_process(delta):
 	
-	var movement = global_transform.origin - to_global(Vector3(0,0,1) * 100)
+	var movement = global_transform.origin - to_global(Vector3(0,0,1) * speed)
 	move_and_slide(movement, Vector3.UP)
 	#print("dfg")
